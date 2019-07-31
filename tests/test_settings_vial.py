d48a9dd24b9891199f2cfa0e46f23d20cf701974
Claudiu Topriceanu <claudiu.topriceanu@kpn.com>
2019-07-31 16:53:21 +0200

    OPT: preserve order for override keys

--- tests/test_settings_vial.py
+++ tests/test_settings_vial.py
@@ -15,9 +15,13 @@ def mock_environment(monkeypatch):
     env_dict = {
         "USER": "test_environment",
         "PREFIX_HASH": '{"dict": "test"}',
+        "PREFIX_VALUE_3": "default-value-3",
         "PREFIX_DEBUG": "true",
         "PREFIX_OVERRIDE_KEY_DEBUG": "false",
         "PREFIX_SOME_KEY_PREFIX_REPEATED": "nested",
+        "PREFIX_OVERRIDE_KEY1_VALUE_1": "key1-value-1",
+        "PREFIX_OVERRIDE_KEY1_VALUE_2": "key1-value-2",
+        "PREFIX_OVERRIDE_KEY2_VALUE_1": "key2-value-1",
     }
 
     for var, value in env_dict.items():
@@ -99,6 +103,26 @@ class TestEnvironmentSettings:
         settings.load_env()
 
         assert settings.SOME_KEY_PREFIX_REPEATED == "nested"
+
+    def test_override_keys_function_preserves_order_of_overrides(
+        self, mock_environment, override_keys_function, mocker
+    ):
+        settings = Settings("PREFIX_", "OVERRIDE_", override_keys_function)
+        mocker.patch.object(settings, 'override_keys_function')
+        settings.override_keys_function.return_value = ("KEY1", "KEY2")
+        settings.load_env()
+        assert settings.VALUE_1 == "key1-value-1"
+        assert settings.VALUE_2 == "key1-value-2"
+        assert settings.VALUE_3 == "default-value-3"
+
+    def test_override_keys_reverse_lookup(self, mock_environment, override_keys_function, mocker):
+        settings = Settings("PREFIX_", "OVERRIDE_", override_keys_function, override_keys_reverse_lookup=True)
+        mocker.patch.object(settings, 'override_keys_function')
+        settings.override_keys_function.return_value = ("KEY1", "KEY2")
+        settings.load_env()
+        assert settings.VALUE_1 == "key2-value-1"
+        assert settings.VALUE_2 == "key1-value-2"
+        assert settings.VALUE_3 == "default-value-3"
 
 
 class TestSettingsTypes:
